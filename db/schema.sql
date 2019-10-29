30af6f393c77998665d70bc7f8d76e54d1d72439
James Robbins <jameslrobbins86@gmail.com>
2019-10-29 00:49:01 -0500

    Trying to get this to work

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,11 @@
 DROP DATABASE burgers_db;
 CREATE DATABASE burgers_db;
 
-USE DATABASE burgers_db;
+USE burgers_db;
 
 CREATE TABLE burgers (
     id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(100),
-    devoured BOOLEAN
-)
+    devoured BOOLEAN,
+    PRIMARY KEY (id)
+);
